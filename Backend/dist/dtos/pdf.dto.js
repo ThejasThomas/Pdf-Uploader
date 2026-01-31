a46f06973e6c399581931b0524265d894b31bc72
Thejas Thomas <thejasthomas001@gmail.com>
2026-01-31 18:55:59 +0530

    Remove dist from repo (gitignore fix)

--- Backend/dist/dtos/pdf.dto.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.extractPdfSchema = void 0;
-const zod_1 = require("zod");
-exports.extractPdfSchema = zod_1.z.object({
-    fileName: zod_1.z.string().min(1, "File name is required"),
-    pages: zod_1.z.array(zod_1.z.number().int().nonnegative())
-});
